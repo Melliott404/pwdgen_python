3de5fe5d5a6fda32ef385f1de0f442185ce80188
John "Marcus" Elliott <156958648+Melliott404@users.noreply.github.com>
2024-02-05 16:00:01 -0800

    Update pwdgen.py
    Added comments

--- pwdgen.py
+++ pwdgen.py
@@ -1,10 +1,13 @@
 import random
 import string
 
+# The int number sets the length of the password given.
+# alphabet cats all the ascii letters, numbers, and symbols to be able to use them all in the generated password
 def generate_password(length: int = 15):
     alphabet = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(random.choice(alphabet) for i in range(length))
     return password
 
+# Calls the password created by the above function.
 password = generate_password()
 print(f"Password: {password}")
